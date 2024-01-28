9c5ed20ace09be7e2f1dd1b071bcb636e6f826e3
Azmain Kabir <kabirswaran792@gmail.com>
2024-01-28 16:34:10 -0500

    completed the getChairs and findResearchLabs methods

--- src/main/java/org/example/Extractor.java
+++ src/main/java/org/example/Extractor.java
@@ -3,6 +3,7 @@ package org.example;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -11,15 +12,52 @@ import java.util.List;
 public class Extractor {
     public List<Chair> getChairs(String url){
 
-        String role, name, email;
+        String role = null, name = null, email = null;
         List<Chair> chairs = new ArrayList<>();
-        //todo
+
+        try {
+            Document document = Jsoup.connect(url).get();
+            Elements allPersons = document.select("tr");
+
+            for (Element person: allPersons) {
+                Elements selectAllPersons = person.select("td");
+                if (!(selectAllPersons.isEmpty()) && selectAllPersons.first().text().toLowerCase().contains("chair")) {
+                    role = selectAllPersons.first().text();
+                    name = person.select("td > strong > a").text();
+                    email = person.select("td > a").text();
+
+                    chairs.add(new Chair(name, role, email));
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return chairs;
     }
     public List<ResearchLab> findResearchLabs(String url) {
 
         List<ResearchLab> researchLabs = new ArrayList<>();
-        //todo
+
+        try {
+            Document document = Jsoup.connect(url).get();
+            Elements allLabs = document.select("li");
+
+            for (Element lab: allLabs) {
+                String labName = lab.select("a").text();
+                String labLink = lab.select("a").attr("href");
+
+                if (labLink.startsWith("research-areas/") && !(labLink.endsWith("/index.html"))) {
+                    if (url.endsWith("index.html")) {
+                        url = url.substring(0, url.length() - "index.html".length());
+                    }
+                    labLink = url + labLink;
+                    researchLabs.add(new ResearchLab(labName, labLink));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return researchLabs;
     }
 
